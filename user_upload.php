9ce82920a745dfed4d6766cb0f23574cd004eed3
Fakhir Mahmood <fakhirm@gmail.com>
2015-09-14 21:56:30 +1000

    second

--- user_upload.php
+++ user_upload.php
@@ -9,13 +9,23 @@ $csvfile=fopen($filename,"r");
 $headings=fgets($csvfile);
 while (!feof($csvfile))
 {
-      $buffer=fgets($csvfile);
+      $buffer=rtrim(fgets($csvfile));
       $arr=explode(",",$buffer);
       if ($arr[0]<>'' && $arr[1]<>'' && $arr[2]<>'')
       {
-         $insert_query="insert into users(name,surname,email) values('$arr[0]','$arr[1]','$arr[2]')";
-         $conn->query($insert_query);
-      }
-      
+         $name=addslashes(ucfirst($arr[0]));
+         $surname=addslashes(ucfirst($arr[1]));
+         $email=addslashes(strtolower($arr[2]));
+         if (!filter_var($email,FILTER_VALIDATE_EMAIL)) 
+         {
+           echo $email." is not a valid email format. This row will be rejected.<br>";
+         }
+         else
+         {
+           $insert_query="insert into users(name,surname,email) values('$name','$surname','$email')";
+           $conn->query($insert_query);
+           echo "Record Inserted.<br>";
+         }
+      } 
 }
 ?>
