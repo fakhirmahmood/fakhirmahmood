fd70efe555abe3e129b0ee79439cc2e11429d968
Fakhir Mahmood <fakhirm@gmail.com>
2015-09-15 21:07:45 +1000

    third

--- user_upload.php
+++ user_upload.php
@@ -1,10 +1,44 @@
 <?php
-define('DBHOST', 'localhost');
-define('DBUSER', 'fakhir');
-define('DBPASS', 'fakhir');
-define('DBNAME', 'fakhir_db');
-$conn=new mysqli(DBHOST,DBUSER,DBPASS,DBNAME);
-$filename="users.csv";
+
+if (isset($_GET['file']))
+   $filename=$_GET['file'];
+if (isset($_GET['u']))
+   $userid=$_GET['u'];
+if (isset($_GET['p']))
+   $password=$_GET['p'];
+if (isset($_GET['h']))
+   $host=$_GET['h'];
+if (isset($_GET['d']))
+   $db=$_GET['d'];
+if (isset($_GET['create_table']))
+   $create_table=$_GET['create_table'];
+if (isset($_GET['dry_run']))
+   $dry_run=$_GET['dry_run'];
+if (isset($_GET['help']))
+   $help=$_GET['help'];
+
+if (isset($host) && isset($userid) && isset($password) && isset($db))
+   $conn=new mysqli($host,$userid,$password,$db);
+
+if ($create_table=="y")
+{
+   $drop_table_query="drop table users";
+   $conn->query($drop_table_query);
+   $create_table_query="create table users (name varchar(100),surname varchar(100),email varchar(100) UNIQUE)";
+   $conn->query($create_table_query);  
+}
+elseif ($help=="y")
+{
+   echo "--file [csv file name] – this is the name of the CSV to be parsed<br>";
+   echo "--create_table – this will cause the MySQL users table to be built (and no further action will be taken)<br>";
+   echo "--dry_run – this will be used with the --file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered<br>";
+   echo "-u – MySQL username<br>";
+   echo "-p – MySQL password<br>";
+   echo "-h – MySQL host<br>";
+   echo "--help – which will output the above list of directives with details";
+}
+else
+{
 $csvfile=fopen($filename,"r");
 $headings=fgets($csvfile);
 while (!feof($csvfile))
@@ -23,9 +57,13 @@ while (!feof($csvfile))
          else
          {
            $insert_query="insert into users(name,surname,email) values('$name','$surname','$email')";
-           $conn->query($insert_query);
+           if (!($dry_run=='y'))
+           {
+              $conn->query($insert_query);
+           } 
            echo "Record Inserted.<br>";
          }
       } 
+}
 }
 ?>
